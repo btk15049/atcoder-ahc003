34848c986163e5fc6a095075562b3ac602b4a629
btk <btk15049@live.jp>
2021-05-27 14:48:36 +0000

    add sleep time option

--- scripts/parallel.fish
+++ scripts/parallel.fish
@@ -5,6 +5,11 @@ if test "$P" = ""
     set P 10
 end
 
+set S $argv[2]
+if test "$S" = ""
+    set S 2
+end
+
 set WORKDIR /tmp/(dbus-uuidgen)
 mkdir $WORKDIR
 echo "parallel.fish: workdir is $WORKDIR"
@@ -21,11 +26,11 @@ for f in (ls in/)
     cargo run --release --bin tester in/$f $BIN 2>$WORKDIR/logs/$f.log | tee $WORKDIR/scores/$f &
     set cnt (math $cnt + 1)
     if test "$cnt" = "$P"
-        sleep 2
+        sleep $S
         set cnt 0
         python3 $REPO_ROOT/py-tools/score.py $WORKDIR/scores
     end
 end
 
-sleep 2
+sleep $S
 echo "score:" (python3 $REPO_ROOT/py-tools/score.py $WORKDIR/scores)
